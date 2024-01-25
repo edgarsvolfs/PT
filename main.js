5aeeb17ff7629990dbd50f8411af7ee9fe4932dc
edgarsvolfs <99868766+edgarsvolfs@users.noreply.github.com>
2024-01-25 21:46:57 +0200

    feat(service): handling multiple data formats

--- main.js
+++ main.js
@@ -1,26 +1,73 @@
 const express = require('express');
+const formidable = require('formidable');
+const xml2js = require('xml2js');
 const app = express();
 
-// Middleware to parse incoming JSON data
 app.use(express.json());
-
 const axios = require('axios');
 
-app.use((req, res, next) => {
-  const startTime = Date.now();
-
-  // Log incoming request
+const logIncomingRequest = (requestData) => {
   const incomingLog = {
     type: 'messageIn',
-    body: req.body,
-    method: req.method,
-    path: req.url,
+    body: requestData,
+    method: 'POST', // or any other default method if needed
+    path: '/', // or any other default path if needed
     dateTime: new Date().toISOString(),
   };
   console.log(JSON.stringify(incomingLog));
+};
 
-  // Continue with the request handling
-  next();
+app.use((req, res, next) => {
+  const contentType = req.get('Content-Type');
+
+  if (contentType === 'application/xml') {
+    let xmlData = '';
+
+    req.on('data', (chunk) => {
+      xmlData += chunk.toString();
+    });
+
+    req.on('end', () => {
+      // Parse XML data to JSON
+      xml2js.parseString(xmlData, (err, result) => {
+        if (err) {
+          return res.status(400).json({ error: 'Error parsing XML data' });
+        }
+        const modifiedResult = {
+          query: result.query,
+          page: Number(result.page)
+        };
+        
+        const resultToString = JSON.stringify(result.request);
+        const modifiedResultToString = resultToString.replace(/[\[\]']+/g, ''); // xml12js converts it to json with [] brackets, this removes it
+        const endResult = JSON.parse(modifiedResultToString);
+        req.body = endResult;
+        logIncomingRequest(req.body);
+        next();
+      });
+    });
+  } 
+  // else if (contentType === 'multipart/form-data') {
+    
+  //   const form = new formidable.IncomingForm();
+  //   //console.log('form', form);
+    
+  //   form.parse(req, (err, fields, files) => {
+  //     console.log('fields', fields);
+  //     if (err) {
+  //       return res.status(400).json({ error: err });
+  //     }
+
+  //     // Convert form data to JSON
+  //     req.body = fields;
+  //     logIncomingRequest(req.body);  
+  //     next();
+  //   });
+  //}
+   else {
+    logIncomingRequest(req.body);
+    next();
+}
 });
 
 
@@ -28,7 +75,6 @@ app.use((req, res, next) => {
 app.post('/', async (req, res) => {
 
   const { query, page } = req.body;
-
   if (typeof query !== 'string' || query.length < 1) {
     const errorResponse = { code: 400, message: 'Invalid or missing query parameter.' };
     return res.status(400).json(errorResponse);
@@ -40,31 +86,27 @@ app.post('/', async (req, res) => {
   }
 
     try {
-        // Construct the API URL using the query and page values
-      const apiUrl = `https://dummyjson.com/products/search?q=${query}&limit=2&skip=${page}`; 
-      //const apiUrl = `https://dummyjson.com/products/wrongEndpoint?q=${query}&limit=2&skip=${page}`;
+      
+      const apiUrl = `https://dummyjson.com/products/search?q=${query}&limit=2&skip=${page}`;
+      //const apiUrl = `https://dummyjson.com/products/wrongEndpoint?q=${query}&limit=2&skip=${page}`; // for error testing
       const response = await axios.get(apiUrl);
       const products = response.data.products;
 
-      //console.log(products);
-
       // Transform the data, e.g., add final_price field
-      const transformedProducts = products.map(product => ({
+      const transformedData = products.map(product => ({
         title: product.title,
         description: product.description,
         final_price: parseFloat((product.price / product.discountPercentage * 100).toFixed(2)),
       }));
   
-      // Log only the relevant information to avoid circular references
-
-      //console.log('Transformed Products:', transformedProducts);
-  
       // Send the transformed data as the response 
-      res.json(transformedProducts); 
+      res.json(transformedData); 
+      
     } catch (error) {
+
       res.locals.error = error; // for js middleware outgoing message
-      //console.error('Error fetching data from the API:', error.message);
-      res.status(error.response.status).json({ code: error.response.status, message: error.stack }); //res.status(500).json({ code: 500, message: error.stack });
+      res.status(error.response.status).json({ code: error.response.status, message: error.stack }); 
+
     } finally {
       // Log outgoing message
       const outgoingLog = {
